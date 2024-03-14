454ca9cf501b549f765443fbab32e4609726e60e
Brajesh420 <brajeshnawhal123@gmail.com>
2024-03-15 03:21:25 +0530

    Ui

---
+++ Assets/Scripts/Ui Scripts/Controls.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour
+{
+    [SerializeField]GameObject controlspanel;
+    [SerializeField] GameObject soundIcon;
+    [SerializeField] Sprite img;
+    [SerializeField] Sprite unMute;
+    [SerializeField] GameObject musicIcon;
+    [SerializeField] Sprite unMutemusic;
+    [SerializeField] Sprite Mutemusic;
+    public void PanelActive()
+    {
+        controlspanel.SetActive(true);
+    }
+
+    public void Backbutton()
+    {
+        controlspanel.SetActive(false);
+    }
+
+    public void MuteButton()
+    {
+        if(soundIcon.GetComponent<Image>().sprite != img)
+        {
+            soundIcon.GetComponent<Image>().sprite = img;
+        }
+        else
+        {
+            soundIcon.GetComponent<Image>().sprite = unMute;
+        }
+
+    }
+
+    public void MusicButton()
+    {
+        if (musicIcon.GetComponent<Image>().sprite != unMutemusic)
+        {
+            musicIcon.GetComponent<Image>().sprite = unMutemusic;
+        }
+        else
+        {
+            musicIcon.GetComponent<Image>().sprite = Mutemusic;
+        }
+
+    }
+}
